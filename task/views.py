32faf1a8c37ef69a81443ce40ff4dbbc642a17d6
Bikram48 <chandbikram001@gmail.com>
2023-03-05 21:49:00 +0545

    implement - task insertion

--- task/views.py
+++ task/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import TaskForm
+from .models import *
 
 # Create your views here.
 def create_task(request):
 
     form = TaskForm()
+    task = Task()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
     context = {
         "form": form
