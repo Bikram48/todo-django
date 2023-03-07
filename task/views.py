b93556e49647d608087e91f1eb4025b4ad13c195
Bikram48 <chandbikram001@gmail.com>
2023-03-07 15:45:31 +0545

    implement - user registration logic

--- task/views.py
+++ task/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from .forms import TaskForm
 from .models import *
 
 # Create your views here.
 def register_user(request):
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
     context = {
-       
+        "form": form
     }
+
     return render(request, "register.html", context)
 
 def create_task(request):
